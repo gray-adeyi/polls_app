f58c89afd8726b69627f5a992c856593eba2cb0a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-11-09 06:47:23 -0500

    Add custom views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,16 @@
 from django.http import HttpResponse
+from . import models
 
 def index(request):
-    return HttpResponse("Hello, Tolu, You're at the polls index.")
+    latest_question_list = models.Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on qustion {question_id}")
