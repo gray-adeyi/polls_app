7d09c5129023d363b9a68dc278274d31dccc5bef
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-11-18 06:09:45 -0500

    Question model list dispaly customization

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from . import models
 
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields':['pub_date']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 # Register your models here.
 admin.site.register(models.Question, QuestionAdmin)
-admin.site.register(models.Choice)
