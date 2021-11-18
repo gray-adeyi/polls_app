ae12acf42bb133fcf33b1faaa2caa0ae45458be0
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-11-18 05:59:58 -0500

    Customize question model admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . import models
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
 # Register your models here.
-admin.site.register(models.Question)
+admin.site.register(models.Question, QuestionAdmin)
