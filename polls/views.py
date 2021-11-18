75871cb664c080cffaceb475e93565e3ae75277b
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-11-18 05:33:03 -0500

    Test for Question model and Index views

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,29 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.views import generic
 from django.urls import reverse
+from django.utils import timezone
 
 from . import models
 
-def index(request):
-    latest_question_list = models.Question.objects.order_by('-pub_date')[:5]
-    context = {
-            'latest_question_list': latest_question_list,
-            }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
-    return render(request, 'polls/detail.html', {
-        'question': question
-    })
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return models.Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = models.Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = models.Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(models.Question, pk=question_id)
